f8ccbac8cd9f2830f1cb09218fe3a601f5e1f26b
Karatino <viewkaratsonic@mail.ru>
2022-09-09 15:21:03 +0500

    Output of the cube table

---
+++ Task_23/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+
+Console.Write("Введите любое число: ");
+int num = int.Parse(Console.ReadLine());
+
+void Cube(int num)
+{
+
+ for (int i = 1; i <= num; i++) 
+
+Console.Write($"{Math.Pow(i, 3)}, ");
+}
+Cube(num);
